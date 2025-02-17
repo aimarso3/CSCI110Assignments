513be0e47c5fd7a599a595a81ab0d54f72c010e9
aimarso3 <aimarso@mavs.coloradomesa.edu>
2025-02-16 20:55:35 -0700

    Add files via upload

---
+++ Ch3.py
@@ -0,0 +1,38 @@
+Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+... 
+... wn= turtle.Screen()
+... 
+... alex=turtle.Turtle()
+... alex.shape('turtle')
+... wn.bgcolor('pink')
+... 
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+SyntaxError: multiple statements found while compiling a single statement
+>>> import turtle
+... 
+... wn= turtle.Screen()
+... 
+... alex=turtle.Turtle()
+... alex.shape('turtle')
+... wn.bgcolor('pink')
+... 
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+... turtle.right(144)
+... turtle.forward(200)
+SyntaxError: multiple statements found while compiling a single statement
